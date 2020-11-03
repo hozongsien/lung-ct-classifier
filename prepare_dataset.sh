4bdf1e0a4558ee814c6feb80fabd638c8e7a32c5
Ho Zong Sien <hozongsien@gmail.com>
2020-11-04 03:32:07 +0800

    Fix rename of image enhancement script

--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -1,8 +1,8 @@
 # remove previously generated dataset
 rm -r ./data/mri_dataset
 
-# pre pre-process images
-python preprocess.py
+# enhance images
+python image_enhancement.py
 
 # prepare tfds dataset
 python -m tensorflow_datasets.scripts.download_and_prepare --datasets=mri_dataset --module_import=datasets.mri_dataset --manual_dir=data/processed --data_dir=data/
